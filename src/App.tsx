68721a6e0a4f7e738d657ba5e7fcfeb4540e1bbf
udithanayanajith <udithanayanajith97@gmail.com>
2025-12-26 15:13:44 +0530

    Create kaveesha branch and push initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
+import Emotion_Landing from "./pages/Emotion_Landing";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />} />
+        <Route path="/emotion_landing" element={<Emotion_Landing />} />
       </Routes>
     </BrowserRouter>
   );
