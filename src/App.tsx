2b4e89707fc9ff16cfa2f1548da1c6cab77c4f1e
udithanayanajith <udithanayanajith97@gmail.com>
2025-12-26 16:05:48 +0530

    Add correct routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Emotion_landing from "./pages/Emotion_landing";
+import Instructions from "./pages/Instructions";
+import EmotionFlow from "./pages/EmotionFlow";
+import Result from "./pages/Result";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/emotion_landing" element={<Emotion_landing />} />
+        <Route path="/instructions" element={<Instructions />} />
+        <Route path="/emotion" element={<EmotionFlow />} />
+        <Route path="/result" element={<Result />} />
       </Routes>
     </BrowserRouter>
   );
