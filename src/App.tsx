ee1f4c25d3d7fbb6f057cdbabe344cf0ce0b97eb
udithanayanajith <udithanayanajith97@gmail.com>
2025-12-26 15:32:39 +0530

    Modify main

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
+import Emotion_landing from "./pages/Emotion_landing";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />} />
+        <Route path="/" element={<Emotion_landing />} />
       </Routes>
     </BrowserRouter>
   );
