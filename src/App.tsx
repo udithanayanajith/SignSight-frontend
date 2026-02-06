d58bd9cf413792a416c923c964a53995fa5e22bc
udithanayanajith <udithanayanajith97@gmail.com>
2026-02-07 00:49:49 +0530

    Modify app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Emotion_landing from "./pages/hasadara/Emotion_Landing";
+
 import MentorLogin from "./pages/kaveesha/MentorLogin";
 import MentorSignUp from "./pages/kaveesha/MentorSignUp";
 import StudentLearningLanding from "./pages/kaveesha/StudentLanding";
@@ -14,7 +14,6 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/emotion_landing" element={<Emotion_landing />} />
 
         <Route path="/student/landing" element={<StudentLearningLanding />} />
         <Route path="/mentor/login" element={<MentorLogin />} />
